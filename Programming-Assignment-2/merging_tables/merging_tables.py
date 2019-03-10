7bfdf44c1972f5163fc9bb8d5b675cf5d1aab6e1
Soe Win Han <hans@berea.edu>
2019-03-10 12:19:01 -0400

    Week 4: hash functions

--- Programming-Assignment-2/merging_tables/merging_tables.py
+++ Programming-Assignment-2/merging_tables/merging_tables.py
@@ -1,31 +1,48 @@
 # python3
-
 import sys
+
 
 n, m = map(int, sys.stdin.readline().split())
 lines = list(map(int, sys.stdin.readline().split()))
 rank = [1] * n
 parent = list(range(0, n))
 ans = max(lines)
+compressor = []
+
 
 def getParent(table):
-    # find parent and compress path
+    global parent, compressor, rank, lines
+    while table != parent[table]:
+        compressor.append(table)
+        table = parent[table]
+    for i in compressor:
+        parent[i] = parent[table]
+        rank[i] = 1
+    compressor = []
     return parent[table]
 
+
 def merge(destination, source):
+    global parent, lines, rank, ans
     realDestination, realSource = getParent(destination), getParent(source)
 
     if realDestination == realSource:
         return False
 
-    # merge two components
-    # use union by rank heuristic 
-    # update ans with the new maximum table size
-    
+    elif rank[realDestination] >= rank[realSource]:
+        parent[realSource] = realDestination
+        lines[realDestination] += lines[realSource]
+        if rank[realDestination] == rank[realSource]:
+            rank[realDestination] += 1
+    else:
+        parent[realDestination] = realSource
+        lines[realSource] += lines[realDestination]
+    ans = max([ans, lines[realDestination], lines[realSource]])
+
     return True
 
 for i in range(m):
     destination, source = map(int, sys.stdin.readline().split())
     merge(destination - 1, source - 1)
     print(ans)
-    
+
