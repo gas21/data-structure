67a87ae17a6a60188b432442980f4513ab7f653b
Soe Win Han <hans@berea.edu>
2019-03-14 02:54:14 -0400

    Update hash_substring.py

--- Programming-Assignment-3/hash_substring/hash_substring.py
+++ Programming-Assignment-3/hash_substring/hash_substring.py
@@ -12,16 +12,21 @@ def print_occurrences(output):
 def get_occurrences(pattern, text):
     hashes, patt, total, index, compare, sol = [], 0, 0, len(pattern), 0, []
     for i in text:
-        hashes.append(hash(i) % 201251)
+        hashes.append(hash(i))
     for i in range(index):
-        patt = (patt + hash(pattern[i])) % 201251
-        compare = (compare + hash(text[i])) % 201251
-    for i in range(len(text)-index):
+        patt = patt + hash(pattern[i])
+        compare = compare + hash(text[i])
+    for i in range(len(text)-index+1):
         if compare == patt:
             if text[i:i+index] == pattern:
                 sol.append(i)
-        compare += hashes[i + index + 1]
+        try:
+            compare = compare + hashes[i + index]
+            compare = compare - hashes[i]
+        except IndexError:
+            break
+    return sol
 
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
